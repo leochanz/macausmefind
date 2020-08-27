e6c757940ee212f27917bda2400930a423880340
leochanz <68209498+leochanz@users.noreply.github.com>
2020-08-27 10:41:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -174,7 +174,7 @@ function dataTransfer(Obj) {
 	console.log(Obj);
 	$('#type2').val(Obj.type);
         $('#name2').val(Obj.name);
-	$('#imageurl2').val(Obj.imageurl);
+
 	$('#latlng2').val(Obj.latlng);
 	$('#address2').val(Obj.address);
 	$('#comment2').val(Obj.comment); 
